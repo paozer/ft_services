96ea0fac15bb0064733b711e96b36cf764e07195
paozer <p.ramellaratin@gmail.com>
2020-04-23 00:35:36 +0200

    cleaned up

--- srcs/wordpress/srcs/setup.sh
+++ srcs/wordpress/srcs/setup.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
-apk add lighttpd php7 php-gettext php-session php7-common php7-iconv php7-json php7-gd php7-curl php7-xml php7-mysqli php7-imap php7-cgi fcgi php7-pdo php7-pdo_mysql php7-soap php7-xmlrpc php7-posix php7-mcrypt php7-gettext php7-ldap php7-ctype php7-dom --no-cache
+apk add lighttpd php7 php-gettext php-session php7-phar php7-common php7-iconv php7-json php7-gd php7-curl php7-xml php7-mysqli php7-imap php7-cgi fcgi php7-pdo php7-pdo_mysql php7-soap php7-xmlrpc php7-posix php7-mcrypt php7-gettext php7-ldap php7-ctype php7-dom --no-cache
 
+# config lighttpd
 sed -i 's/#   include "mod_fastcgi.conf"/include "mod_fastcgi.conf"/g' /etc/lighttpd/lighttpd.conf
 sed -i 's/# server.port          = 81/server.port = 5050/g' /etc/lighttpd/lighttpd.conf
 sed -i 's/usr\/bin\/php-cgi/usr\/bin\/php-cgi7/g' /etc/lighttpd/mod_fastcgi.conf
