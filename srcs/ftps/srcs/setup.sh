6127d4e50e985d45b4c8d520f74446659fd06c8b
paozer <p.ramellaratin@gmail.com>
2020-04-25 01:58:54 +0200

    connection while inside cluster is working; no up/downloads

--- srcs/ftps/srcs/setup.sh
+++ srcs/ftps/srcs/setup.sh
@@ -2,32 +2,16 @@
 
 apk add openssl vsftpd --no-cache
 
-## add ssh user
-#adduser -D ssh_dummy
-#echo "ssh_dummy:password" | chpasswd
-## create home dir for chroot jail
-#mkdir /home/ssh_dummy/ftp
-#chown nobody:nogroup /home/ssh_dummy/ftp
-#chmod a-w /home/ssh_dummy/ftp
-## create files dir for uploading
-#mkdir /home/ssh_dummy/ftp/files
-#chown ssh_dummy:ssh_dummy /home/ssh_dummy/ftp/files
-## add test file
-#echo "blablablabla" > /home/ssh_dummy/ftp/files/test.txt
-## add user list
-#touch etc/vsftpd/vsftpd.userlist
-#echo "ssh_dummy" >> etc/vsftpd/vsftpd.userlist
-
 echo "## Adding user"
-mkdir -p /ftps/data/admin
-echo "test shit" > /ftps/data/admin/test.txt
-adduser -D -h /ftps/data admin
-chown admin /ftps/data/admin
+mkdir -p /home/vsftpd/admin/
+echo "test" > /home/vsftpd/admin/test.txt
+adduser -D -h /home/vsftpd/admin admin
+chown -R ftp:ftp /home/vsftpd/
+chown -R admin /home/vsftpd/admin
 echo "admin:password" | chpasswd
 echo "admin" >> etc/vsftpd/vsftpd.userlist
 
 #touch /etc/vsftpd/vsftpd.chroot_list
-#echo "admin" > /etc/vsftpd/vsftpd.userlist
 touch /var/log/vsftpd.log
 
 mkdir -p /etc/ssl/private
