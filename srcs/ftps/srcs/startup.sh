05c8d5ecaf16b9e8ff37835b123a970b8659449f
paozer <p.ramellaratin@gmail.com>
2020-04-26 01:52:11 +0200

    added working ftps lacks security

--- srcs/ftps/srcs/startup.sh
+++ srcs/ftps/srcs/startup.sh
@@ -1,3 +1,18 @@
 #!/bin/sh
 
+mkdir -p /ftps/admin
+adduser --home=/ftps/admin -D admin
+mkdir -p /ftps/admin/uploads
+chmod 777 /ftps/admin/uploads
+chown nobody:nogroup /ftps
+chmod a-w /ftps
+
+echo "admin:password" | chpasswd
+echo "admin" >> etc/vsftpd/vsftpd.userlist
+
+touch /var/log/vsftpd.log
+
+mkdir -p /etc/ssl/private
+openssl req -x509 -nodes -days 365 -newkey rsa:4096 -keyout /etc/ssl/private/vsftpd.pem -out /etc/ssl/private/vsftpd.pem -config etc/ssl/private/openssl.conf
+
 /usr/sbin/vsftpd /etc/vsftpd/vsftpd.conf
